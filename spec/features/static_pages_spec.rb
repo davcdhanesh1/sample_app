2f4f6ecc17cbf167566f5224e47275aca24a66cd
davcdhanesh1 <davcdhanesh1@gmail.com>
2014-07-19 15:59:48 +0530

    using path variable instead of url

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -11,48 +11,48 @@ RSpec.describe 'StaticPages', :type => :feature do
                         This app is just to demonstrate how to develop using Ruby on Rails
                         Sign up now!
                         This site is maintained by Tripartite Inc. @copyright 2016'
-        visit static_pages_home_path
+        visit home_path
         expect(page).to have_content(expected_msg)
       end
 
       it "should have title as 'sample app | Home' " do
         expected_title = "#{base_title} Home"
-        visit static_pages_home_url
+        visit home_path
         expect(page).to have_title(expected_title)
       end
     end
 
     describe 'GET /help page' do
       it 'should load help page and display help center no' do
-        visit static_pages_help_path
+        visit help_path
         expected_msg = 'This is help page for this Sample App'
         expect(page).to have_content(expected_msg)
       end
 
       it "should have title as 'sample app | Help' " do
         expected_title = "#{base_title} Help"
-        visit static_pages_help_path
+        visit help_path
         expect(page).to have_title(expected_title)
       end
     end
 
     describe 'GET /about page' do
       it 'should load about page and display my profile' do
-        visit 'static_pages/about'
+        visit about_path
         expected_msg = 'This is about page'
         expect(page).to have_content(expected_msg)
       end
 
       it "should have title as 'sample app | About' " do
         expected_title = "#{base_title} About"
-        visit static_pages_about_path
+        visit about_path
         expect(page).to have_title(expected_title)
       end
     end
 
     describe 'GET /contact page' do
       it 'should load contact page and display contact info' do
-        visit 'static_pages/contact'
+        visit contact_path
         expected_msg = 'This is contact page'
         expect(page).to have_content(expected_msg)
       end
