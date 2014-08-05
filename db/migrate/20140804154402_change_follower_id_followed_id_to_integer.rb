2765eb75627574f98e3b723725c2a547f2d9790e
davcdhanesh1 <davcdhanesh1@gmail.com>
2014-08-05 07:09:53 +0530

    psql specific casting of columns

--- db/migrate/20140804154402_change_follower_id_followed_id_to_integer.rb
+++ db/migrate/20140804154402_change_follower_id_followed_id_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeFollowerIdFollowedIdToInteger < ActiveRecord::Migration
   def change
-    change_column :relationships, :followed_id, :integer
-    change_column :relationships, :follower_id, :integer
+    change_column :relationships, :followed_id, 'integer USING CAST(followed_id AS integer)'
+    change_column :relationships, :follower_id, 'integer USING CAST(follower_id AS integer)'
   end
 end
